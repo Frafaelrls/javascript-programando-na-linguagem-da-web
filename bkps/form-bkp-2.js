2d4f3226e05ba977a5d454074d58fd307f606781
Frafaelrls <frafaelrls@gmail.com>
2023-02-01 17:04:02 -0300

    criando o arquivo form-bkp-2.js

---
+++ bkps/form-bkp-2.js
@@ -0,0 +1,72 @@
+//------------------------------------------------------
+// Manipulação do formulário, adicionando novos pacientes - Usado até o final da aula 5.11
+//------------------------------------------------------
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+
+// Adicionando a escuta de eventos (addEventListener) 
+// A função utilizada é uma função anónima, não possui nome
+botaoAdicionar.addEventListener('click', function (event) {
+    // event recebe o evento esperado, neste caso recebe o evento 'click'
+    // função que impede que o comportamento padrão de eventos ocorra
+    event.preventDefault();
+
+    // Selecionando o formulário
+    var form = document.querySelector('#form-adiciona');
+    
+    // Extraindo informações do paciente do form
+    var paciente = obtemPacienteDoFormulario(form);
+
+    // ciar a tr e td do paciente
+    var pacienteTr = montarTr(paciente);
+
+    // Adicionando a linha(tr) a tabela(tbody)
+    // Uma linha é filha de uma tabela, será adicionado usando a função appendChild() 
+    var tabela = document.querySelector('#tabela-pacientes');
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+
+});
+
+function obtemPacienteDoFormulario(form){
+    
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    } 
+    // retorna um objeto
+    return paciente;
+ 
+}
+
+function montarTr(paciente){
+
+    // Criando uma linha para a tabela(tr) 
+    var pacienteTr = document.createElement('tr');
+    pacienteTr.classList.add('paciente');
+
+    // Adicionando as células(td) a linha(tr)
+    // Uma célula é filha de uma linha, será adicionado usando a função appendChild()
+    pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'));
+    pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'));
+    pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'));
+    pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'));
+    pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'));
+     
+     return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    // Criando as células(td) para a tabela
+    var td = document.createElement('td');
+    // Adicionando os dados as células(td)
+    td.textContent = dado;
+    // Adicionando a classe css
+    td.classList.add(classe);
+    
+    return td;
+}
