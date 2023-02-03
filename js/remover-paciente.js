2493ac3328b013487137bb4cfe45a4bf0b1a0daa
Frafaelrls <frafaelrls@gmail.com>
2023-02-03 14:38:41 -0300

    adicionando tempo de espera antes da remoção do paciente e adicionando classe fadeOut ao paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,7 +3,19 @@ var tbody = document.querySelector('tbody');
 tbody.addEventListener('dblclick', function(event){
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover
+    
     var nomePaciente = paiDoAlvo.querySelector('.info-nome').textContent;
+    
+    // Janela de confirmação
     var resposta = window.confirm(`Deseja deletar o(a) paciente ${nomePaciente}?`)
-    if(resposta) paiDoAlvo.remove();
+    
+    if(resposta) {
+        paiDoAlvo.classList.add('fadeOut');
+        
+        // Função par aadicionar o tempo de espera
+        setTimeout(function(){
+            paiDoAlvo.remove();
+        }, 500)
+
+    }
 });
