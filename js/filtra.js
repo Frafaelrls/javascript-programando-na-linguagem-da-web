a3e1ac3ae3daf6c8f04ac138bf3b80ab8c2c91cd
Frafaelrls <frafaelrls@gmail.com>
2023-02-17 11:12:01 -0300

    criando logica para a adição e reomoção da classe invisivel nas tags tr

--- js/filtra.js
+++ js/filtra.js
@@ -1,15 +1,27 @@
 var campoFiltro = document.querySelector('#filtrar-tabela');
 
-campoFiltro.addEventListener('input', function(){
-    
+campoFiltro.addEventListener('input', function () {
+
     console.log(this.value);
     var pacientes = document.querySelectorAll('.paciente');
-    for(var i = 0 ; i < pacientes.length; i++){
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector('.info-nome');
-        var nome = tdNome.textContent;
-        console.log(nome);
-        
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector('.info-nome');
+            var nome = tdNome.textContent;
+
+            if (nome != this.value) {
+                // Ao adicionar a classe utilizar apenas o nome da classe, não utilizar o acessor ponto (.)
+                paciente.classList.add('invisivel');
+            } else {
+                paciente.classList.remove('invisivel');
+            }
+
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove('invisivel');
+        }
     }
-    
 });
