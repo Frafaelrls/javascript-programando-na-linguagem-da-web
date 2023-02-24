a1d9fd0147c6491c69e7d69bed5d041e994dd479
Frafaelrls <frafaelrls@gmail.com>
2023-02-24 14:30:17 -0300

    adicionando a validação da resposta do request

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -12,15 +12,25 @@ buscarPacientes.addEventListener('click', function () {
     // Adicionando um exento a requisição, neste caso informando que ao receber a requisição
     // Deve executar a função
     xhr.addEventListener('load', function () {
-        var resposta = xhr.responseText;
+        var erroAjax = document.querySelector('#erro-ajax');
+        if (xhr.status == 200){
+            erroAjax.classList.add('invisivel');
+            var resposta = xhr.responseText;
 
-        // Convertendo JSON para um objeto javaScript, nste caso para um array
-        var pacientes = JSON.parse(resposta);
+            // Convertendo JSON para um objeto javaScript, nste caso para um array
+            var pacientes = JSON.parse(resposta);
+    
+            // Adicionando os pacientes na tabela
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            erroAjax.classList.remove('invisivel');
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+       
 
-        // Adicionando os pacientes na tabela
-        pacientes.forEach(function (paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
     });
 
     // Enviando a requisição
