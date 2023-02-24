0f882c398a4f3e8bf43d1f1fee9bc0ea0a5a9482
Frafaelrls <frafaelrls@gmail.com>
2023-02-24 10:29:54 -0300

    adicionando a conversão de JSON para Array e adicionado a chamada da função adicionaPacienteNaTabela

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,18 +1,26 @@
 var buscarPacientes = document.querySelector('#buscar-pacientes');
 
-buscarPacientes.addEventListener('click', function(){
-    
+buscarPacientes.addEventListener('click', function () {
+
     // Criando uma nova requisição 
     var xhr = new XMLHttpRequest();
-    
+
     // A função 'open' abre a requisição com um determinaod endereço. Informamos o tipo, neste caso GET
     // O segundo parâmetro será o endereço
     xhr.open('GET', 'https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json');
 
     // Adicionando um exento a requisição, neste caso informando que ao receber a requisição
     // Deve executar a função
-    xhr.addEventListener('load', function(){
-        console.log(xhr.responseText);
+    xhr.addEventListener('load', function () {
+        var resposta = xhr.responseText;
+
+        // Convertendo JSON para um objeto javaScript, nste caso para um array
+        var pacientes = JSON.parse(resposta);
+
+        // Adicionando os pacientes na tabela
+        pacientes.forEach(function (paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
     });
 
     // Enviando a requisição
